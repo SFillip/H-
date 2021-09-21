8613de70f9d2fff54b1933c19b4351e2cc1082e5
Christian Sturm <Itsab338@gmail.com>
2021-09-21 15:34:55 +0200

    ErastosthenesPrimeSieveAlgorythemInclusivMethodsComid

--- 1. Hausübung/src/main/java/net/htlgkr/FillipS190052/PrimeSieve/EratosthenesPrimeSieve.java
+++ 1. Hausübung/src/main/java/net/htlgkr/FillipS190052/PrimeSieve/EratosthenesPrimeSieve.java
@@ -12,34 +12,44 @@ public class EratosthenesPrimeSieve implements PrimeSieve {
 
     @Override
     public boolean isPrime(int p) {
-        return false;
+        boolean[] primes = getAllPrimes();
+
+        if(primes[p]==true)
+        {
+            return true;
+        }else {
+            return  false;
+        }
     }
 
     @Override
     public void printPrimes() {
+        boolean[] primes = getAllPrimes();
 
+        for (int i = 0; i < primes.length; i++) {
+            if (primes[i] == true) {
+                System.out.println(i);
+            }
+        }
     }
 
-    public boolean[]getAllPrimes(){
-        boolean[]primes=new boolean[upperBorder];
+    public boolean[] getAllPrimes() {
+        boolean[] primes = new boolean[upperBorder];
 
-        for(int i=1;i<primes.length;i++)
-        {
-            primes[i]=true;
+        for (int i = 1; i < primes.length; i++) {
+            primes[i] = true;
         }
 
-        for(int i=1;i<primes.length;i++)
-        {
-            if(primes[i]==false){
+        for (int i = 1; i < primes.length; i++) {
+            if (primes[i] == false) {
 
-            }else{
-                for(int j=i*i;i<primes.length;j+j)
-                {
-                    primes[j]=false;
+            } else {
+                for (int j = i * i; i < primes.length; j + j) {
+                    primes[j] = false;
                 }
             }
 
         }
-
+        return primes;
     }
 }
