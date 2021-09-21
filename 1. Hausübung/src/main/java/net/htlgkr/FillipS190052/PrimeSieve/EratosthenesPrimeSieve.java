c54409b9fad563b61be5516183470ffadf0c94bb
Christian Sturm <Itsab338@gmail.com>
2021-09-21 15:23:15 +0200

    AlgorythemCommit

--- 1. Hausübung/src/main/java/net/htlgkr/FillipS190052/PrimeSieve/EratosthenesPrimeSieve.java
+++ 1. Hausübung/src/main/java/net/htlgkr/FillipS190052/PrimeSieve/EratosthenesPrimeSieve.java
@@ -1,11 +1,13 @@
 package net.htlgkr.FillipS190052.PrimeSieve;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EratosthenesPrimeSieve implements PrimeSieve {
     private int upperBorder;
 
-    public EratosthenesPrimeSieve(int upperBorder)
-    {
-        this.upperBorder=upperBorder;
+    public EratosthenesPrimeSieve(int upperBorder) {
+        this.upperBorder = upperBorder;
     }
 
     @Override
@@ -15,6 +17,29 @@ public class EratosthenesPrimeSieve implements PrimeSieve {
 
     @Override
     public void printPrimes() {
-    
+
+    }
+
+    public boolean[]getAllPrimes(){
+        boolean[]primes=new boolean[upperBorder];
+
+        for(int i=1;i<primes.length;i++)
+        {
+            primes[i]=true;
+        }
+
+        for(int i=1;i<primes.length;i++)
+        {
+            if(primes[i]==false){
+
+            }else{
+                for(int j=i*i;i<primes.length;j+j)
+                {
+                    primes[j]=false;
+                }
+            }
+
+        }
+
     }
 }
