b189d7fe5a83468d1026fef4f11e910c8aafdc14
SFillip <Itsab338@gmail.com>
2021-09-28 13:51:25 +0200

    InitialCommit of 3. Task

---
+++ 2. Hausübung/src/main/java/net/htlgkr/sfillip19/Ho2/Bsp3/NumberTester.java
@@ -0,0 +1,12 @@
+package net.htlgkr.sfillip19.Ho2.Bsp3;
+
+public class NumberTester {
+    public NumberTester(String fileName){
+
+    }
+
+    public void setOddEvenTester(NumberTester oddTester)
+    {
+        
+    }
+}
