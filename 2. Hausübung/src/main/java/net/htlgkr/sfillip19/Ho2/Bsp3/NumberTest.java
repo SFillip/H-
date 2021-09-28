df4acce7880b60461850896f8efdc60455e41afb
SFillip <Itsab338@gmail.com>
2021-09-28 14:12:47 +0200

    Commit of NumberTester Constructor

---
+++ 2. Hausübung/src/main/java/net/htlgkr/sfillip19/Ho2/Bsp3/NumberTest.java
@@ -0,0 +1,5 @@
+package net.htlgkr.sfillip19.Ho2.Bsp3;
+
+public interface NumberTest {
+    boolean testNumber(int number);
+}
--- 2. Hausübung/src/main/java/net/htlgkr/sfillip19/Ho2/Bsp3/NumberTester.java
+++ 2. Hausübung/src/main/java/net/htlgkr/sfillip19/Ho2/Bsp3/NumberTester.java
@@ -1,12 +1,51 @@
 package net.htlgkr.sfillip19.Ho2.Bsp3;
 
-public class NumberTester {
-    public NumberTester(String fileName){
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class NumberTester {
+
+    private NumberTest oddTester;
+    private NumberTest printTester;
+    private NumberTest paindroneTester;
+
+    private int rowCount;
+    private String[]rows;
+
+    public NumberTester(String fileName) {
+
+        Scanner scanner = null;
+
+        try {
+            scanner = new Scanner(new File(fileName));
+        } catch (FileNotFoundException e) {
+            System.out.println("Datei existiert nicht");
+            System.exit(0);
+        }
+
+        rowCount = Integer.parseInt(scanner.next());
+
+        rows=new String[rowCount];
+
+        for(int i=0;i<rowCount;i++){
+            rows[i]=scanner.next();
+        }
     }
 
-    public void setOddEvenTester(NumberTester oddTester)
-    {
-        
+    public void setOddTester(NumberTest oddTester) {
+        this.oddTester = oddTester;
+    }
+
+    public void setPrintTester(NumberTest printTester) {
+        this.printTester = printTester;
+    }
+
+    public void setPaindroneTester(NumberTest paindroneTester) {
+        this.paindroneTester = paindroneTester;
+    }
+
+    public void TestFile() {
+
     }
 }
